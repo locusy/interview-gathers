b6b4f1fabe8c4aced1555d6d9faece2bd6328c41
tianzhi <1095098525@qq.com>
2020-05-23 10:38:03 +0800

    算法

--- src/components/Algorithm/NoRepeat.vue
+++ src/components/Algorithm/NoRepeat.vue
@@ -14,7 +14,7 @@
     // https://juejin.im/post/5c7e0328f265da2dc538da03
 
     // 1、双重for循环
-    var arr1 = [1,2,3,3,3,4,4,5];
+    var arr1 = [1,2,3,3,3,4,4,5]
     function noRepeat(arr1) {
         for(var i=0;i<arr1.length;i++){
             for(var j=i+1;j<arr1.length;j++){
@@ -41,7 +41,7 @@
     // }
     // console.log(unique(arr))
 
-    // 3、使用includes()
+    // 3、使用includes
     // var arr = [1,2,3,3,3,4,4,5]
     // function unique1(arr) {
     //     let res = []
@@ -63,7 +63,7 @@
     // }
     // console.log(unique2(arr))
     
-    // 5、使用对象的特点：
+    // 5、使用对象的特点
     // var arr = [1,2,3,3,3,4,4,5]
     // function unique3(arr) {
     //     let obj = {}
@@ -78,7 +78,7 @@
     // }
     // console.log(unique3(arr))
 
-    // 6、使用set：
+    // 6、使用set
     // var arr = [1,2,3,3,3,4,4,5]
     // function unique4(arr) {
     //     return [...new Set(arr)]
