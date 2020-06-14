6c98698b8d0b9e9b174853750de7e659f3991be8
tianzhi <1095098525@qq.com>
2020-06-14 22:07:01 +0800

    new

--- src/components/Javascript/index.vue
+++ src/components/Javascript/index.vue
@@ -919,6 +919,9 @@
           inner-setTimeout---0
 
   33、js是如何构造抽象语法树（AST）的？
+      抽象语法树（Abstract Syntax Tree）也称为AST语法树，指的是源代码语法所对应的树状结构。
+      也就是说，对于一种具体编程语言下的源代码，通过构建语法树的形式将源代码中的语句映射到树中的每一个节点上。 
+
       https://www.cnblogs.com/fundebug/p/how-does-javascript-compile.html
       https://segmentfault.com/a/1190000016231512
 
